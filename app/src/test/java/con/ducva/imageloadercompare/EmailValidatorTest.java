1ab2b13759be73d011b1ff2c99824f9751b9b870
hex0r <hex0r@me.com>
2017-02-20 22:53:03 +0700

    Add unit test

---
+++ app/src/test/java/con/ducva/imageloadercompare/EmailValidatorTest.java
@@ -0,0 +1,51 @@
+package con.ducva.imageloadercompare;
+
+import org.junit.Test;
+
+import con.ducva.imageloadercompare.utils.EmailValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by HAL Team on 2/20/17.
+ * Copyright © 2017 GMO Media Inc. All rights reserved.
+ */
+public class EmailValidatorTest {
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"));
+    }
+
+    @Test
+    public void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""));
+    }
+
+    @Test
+    public void emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null));
+    }
+
+}
